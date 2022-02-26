22795d876091c3a2e4b0b71e78b34d04aaa2154b
Blandaren123 <toskha_91@hotmail.se>
2022-02-26 16:58:56 +0000

    add if i can approve comments

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-
+"""models for admin"""
     list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
     list_filter = ('status', 'created_on')
@@ -15,6 +15,9 @@ class PostAdmin(SummernoteModelAdmin):
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    """
+    Class for comment in django adminpanel
+    """
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
