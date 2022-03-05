c55d16b1d92c90ca3c14f73bdfcf33d1951bb36c
Blandaren123 <toskha_91@hotmail.se>
2022-03-05 19:40:58 +0000

    add author when post

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 #from . import views
-from .views import HomeView
+from .views import HomeView, ArticleDetailView
 
 
 urlpatterns = [
     #path('', views.home, name='home'),
     path('', HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,7 @@ from.models import Post
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+
+class ArticleDetailView(DetailView):
+    model = Post
+    template_name = 'article_details.html'
