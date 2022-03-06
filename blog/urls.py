c7ecaf49a3c0a92385c91e443b9002c7bd48cfe0
Blandaren123 <toskha_91@hotmail.se>
2022-03-06 12:50:46 +0000

    add like and unlike Button

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView
+from .views import HomeView, ArticleDetailView, AddPostView, UpdatePostView, DeletePostView, LikeView
 
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
+    path('like/<int:pk>', LikeView, name='like_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from.models import Post
 from .forms import PostForm, EditForm
-from django.urls import reverse_lazy 
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 
 # def home(request):
 #    return render(request, 'home.html', {})
+def LikeView(request, pk):
+    post = get_object_or_404(Post, id=request.POST.get('post_id'))
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+        
+    return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
     ordering = ['-post_date']
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        return context
+
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
+        
+        stuff = get_object_or_404(Post, id=self.kwargs['pk'])
+        total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        context["total_likes"] = total_likes
+        context["liked"] = liked
+        return context
+        
 
 class AddPostView(CreateView):
     model = Post
