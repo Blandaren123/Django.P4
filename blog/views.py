2b4a78ea934aef432e1b818f17838f3d06246300
Blandaren123 <toskha_91@hotmail.se>
2022-03-06 03:28:52 +0000

    add a add post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from.models import Post
 
 # def home(request):
@@ -12,3 +12,8 @@ class HomeView(ListView):
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_details.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
