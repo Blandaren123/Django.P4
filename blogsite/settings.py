4adc5629a979b0e2cd43e4c563c5359eba5a25e3
Blandaren123 <toskha_91@hotmail.se>
2022-03-06 06:41:30 +0000

    add Login and registration

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
     'django_summernote',
     'crispy_forms',
     'blog',
+    'members',
 ]
 
 SITE_ID = 1
--- blogsite/urls.py
+++ blogsite/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('blog.urls'), name='blog_urls'),
-    path('accounts/', include('allauth.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
