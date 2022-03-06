13d3663268dbafeacd1987d12cd7e56613c658f2
Blandaren123 <toskha_91@hotmail.se>
2022-03-06 11:04:22 +0000

    cannot pick who is the owner of the post

--- blog/forms.py
+++ blog/forms.py
@@ -10,7 +10,8 @@ class PostForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.Select(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'user', 'type':'hidden'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
