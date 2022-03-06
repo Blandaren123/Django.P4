55b0d4e13665717af249f82ee9ec94d4015367f1
Blandaren123 <toskha_91@hotmail.se>
2022-03-06 20:10:37 +0000

    fixed Like button bugg

--- blog/urls.py
+++ blog/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
     path('article/<int:pk>/delete', DeletePostView.as_view(), name='delete_post'),
-    path('article/<int:pk>', LikeView, name='like_post'),
+    path('like/<int:pk>', LikeView, name='like_post'),
     path('article/<int:pk>/comment/', AddCommentView.as_view(), name='add_comment'),
 ]
--- blogsite/settings.py
+++ blogsite/settings.py
@@ -68,7 +68,7 @@ MESSAGE_TAGS = {
     messages.SUCCESS: 'alert-success',
     messages.WARNING: 'alert-warning',
     messages.ERROR: 'alert-danger',
-    }
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
