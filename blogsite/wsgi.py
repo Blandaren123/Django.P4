3dd336fe6467470d77a26976971993ef6015dd01
Blandaren123 <toskha_91@hotmail.se>
2022-03-07 20:46:53 +0000

    add deplytment new

--- blogsite/wsgi.py
+++ blogsite/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DJANGO.P4.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogsite.settings')
 
 application = get_wsgi_application()
+
